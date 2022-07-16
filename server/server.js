627350bad93cbd2a45a8a956af3274fa6a05a1a2
Maxim Chen <chenr.maxim@gmail.com>
2022-07-15 21:31:54 -0700

    add: db & server functionality

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = require('../config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true})
+  .then(() => console.log('connected to mongodb'))
+  .catch(err => console.log(err));
+
+app.listen(5000, function() {
+  console.log('listening on 5000');
+})
+
+const routes = require('./routes/routes');
+app.use('/api', routes);
